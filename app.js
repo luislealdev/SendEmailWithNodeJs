2e041177b0bcd1083e794b2bf217a03648f9065b
Luis Leal <luisrrleal@gmail.com>
2022-10-20 20:14:23 -0500

    Now it sends images

--- app.js
+++ app.js
@@ -1,34 +1,81 @@
- //Requerimos el paquete
- var nodemailer = require('nodemailer');
- require('dotenv').config();
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const multer = require("multer");
+const express = require("express");
 
- //Creamos el objeto de transporte
- var transporter = nodemailer.createTransport({
-   service: 'gmail',
-   auth: {
-     user: 'luisrrleal@gmail.com',
-     pass: process.env.PASS
-   }
- });
- 
- var mensaje = "Hola desde nodejs... si estás viendo esto es porque la prueba funcionó.";
- 
- var mailOptions = {
-   from: 'tucorreo@gmail.com',
-   to: 'visualcenter.mkt@gmail.com',
-   subject: 'Esta es una prueba',
-   text: mensaje,
-   attachments: [
-    {   // utf-8 string as an attachment
-        filename: 'text1.txt',
-        content: 'hello world!'
-    }]
- };
- 
- transporter.sendMail(mailOptions, function(error, info){
-   if (error) {
-     console.log(error);
-   } else {
-     console.log('Email enviado: ' + info.response);
-   }
- });
+var path1;
+var path2;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const Storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./files");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
+});
+
+var upload = multer({
+  storage: Storage,
+}).array("files");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  upload(req, res, function (err) {
+    if (err) {
+      console.log(err);
+      return res.end("Something went wrong!");
+    } else {
+      path1 = req.files[0].path;
+      console.log(path1);
+      path2 = req.files[1].path;
+      console.log(path2);
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "luisrrleal@gmail.com",
+          pass: process.env.PASS,
+        },
+      });
+
+      var mensaje =
+        "Hola desde nodejs... si estás viendo esto es porque la prueba funcionó.";
+
+      var mailOptions = {
+        from: "luisrrleal@gmail.com",
+        to: "visualcenter.mkt@gmail.com",
+        subject: "Esta es una prueba",
+        text: mensaje,
+        attachments: [
+        //   {
+        //     path: path,
+        //   },
+          {
+            path: path1,
+          },
+          {
+            path: path2,
+          },
+        ],
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email enviado: " + info.response);
+        }
+      });
+    }
+  });
+});
+app.listen(4000, function () {
+  console.log("Server is on");
+});
